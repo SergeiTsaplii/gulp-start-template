151d7046235524091840867092ff3dd849866f40
SergeiTsaplii <tsaplii98@gmail.com>
2023-12-31 14:01:41 +0200

    create and configure function images

--- gulp/config/paths.js
+++ gulp/config/paths.js
@@ -7,16 +7,20 @@ const filePaths = {
   build: {
     css: `${buildFolder}/css/`,
     js: `${buildFolder}/js/`,
+    images: `${buildFolder}/images/`,
   },
   src: {
     html: `${srcFolder}/html/*.html`,
     scss: `${srcFolder}/scss/*.scss`,
     js: `${srcFolder}/js/*.js`,
+    images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,tiff}`,
+    svg: `${srcFolder}/images/**/*.svg`,
   },
   watch: {
     html: `${srcFolder}/html/**/*.html`,
     scss: `${srcFolder}/scss/**/*.scss`,
     js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,svg,tiff}`,
   },
   buildFolder,
   srcFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import server from './gulp/tasks/server.js';
 import html from './gulp/tasks/html.js';
 import scss from './gulp/tasks/scss.js';
 import js from './gulp/tasks/js.js';
+import images from './gulp/tasks/images.js';
 import filePaths from './gulp/config/paths.js';
 
 const { parallel, series, watch } = pkg;
@@ -12,14 +13,16 @@ const isBuild = process.argv.includes('--build');
 const handleHTML = html.bind(null, isBuild);
 const handleSCSS = scss.bind(null, isBuild);
 const handleJS = js.bind(null, !isBuild);
+const handleImages = images.bind(null, isBuild);
 
 const watcher = () => {
   watch(filePaths.watch.html, handleHTML);
   watch(filePaths.watch.scss, handleSCSS);
   watch(filePaths.watch.js, handleJS);
+  watch(filePaths.watch.images, handleImages);
 };
 
-const mainTasks = parallel(handleHTML, handleSCSS, handleJS);
+const mainTasks = parallel(handleHTML, handleSCSS, handleJS, handleImages);
 
 const dev = series(clean, mainTasks, parallel(watcher, server));
 const build = series(clean, mainTasks);
