b1a972bc953ac0e0a63d2b59aa160c070c5cb06e
SergeiTsaplii <tsaplii98@gmail.com>
2024-01-01 18:31:11 +0200

    update task favicon

--- gulp/tasks/favicon.js
+++ gulp/tasks/favicon.js
@@ -1,28 +1,40 @@
-import pkg from 'gulp';
-import favicons from 'gulp-favicons';
-import filter from 'gulp-filter';
-import filePaths from '../config/paths.js';
-import logger from '../config/Logger.js';
+/* eslint-disable no-return-await */
+import favicons from 'favicons';
+import fs from 'fs/promises';
+import path from 'path';
 
-const { src, dest } = pkg;
+const src = './src/favicon/favicon.svg'; // Icon source file path.
+const dest = './src/images/favicons'; // Output directory path.
+const htmlBasename = 'index.html'; // HTML file basename.
 
-const favicon = () => src(filePaths.src.favicon)
-  .pipe(logger.handleError('FAVICON'))
-  .pipe(favicons({
-    icons: {
-      favicons: true,
-      appleIcon: true,
-      android: true,
-      windows: false,
-      yandex: false,
-      coast: false,
-      firefox: false,
-      appleStartup: false,
-    },
-    path: 'images/favicons/',
-  }))
-  .pipe(dest(`${filePaths.srcFolder}/images/favicons`))
-  .pipe(filter(['favicon.ico', 'apple-touch-icon.png', 'favicon.svg', 'manifest.webmanifest']))
-  .pipe(dest(filePaths.srcFolder));
+// Configuration (see above in the README file).
+const configuration = {
+  path: '/images/favicons',
+  icons: {
+    favicons: true,
+    appleIcon: true,
+    android: true,
+    windows: false,
+    yandex: false,
+    coast: false,
+    firefox: false,
+    appleStartup: false,
+  },
+  // Extra options...
 
-export default favicon;
+};
+
+// Below is the processing.
+const response = await favicons(src, configuration);
+await fs.mkdir(dest, { recursive: true });
+await Promise.all(
+  response.images.map(
+    async (image) => await fs.writeFile(path.join(dest, image.name), image.contents),
+  ),
+);
+await Promise.all(
+  response.files.map(
+    async (file) => await fs.writeFile(path.join(dest, file.name), file.contents),
+  ),
+);
+await fs.writeFile(path.join(dest, htmlBasename), response.html.join('\n'));
