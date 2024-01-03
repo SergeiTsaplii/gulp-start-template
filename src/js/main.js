d5d2ec9a453d262d1d1551b504cb9e9b22a40a67
SergeiTsaplii <tsaplii98@gmail.com>
2024-01-03 15:28:12 +0200

    configure base structure

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'gulp-avif-css-fix/plugin';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'focus-visible';
